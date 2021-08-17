dd1866f744f50c17b953a54ca116cb666d796a8e
AyanoOkamuro <omochi185@gmail.com>
2021-08-17 15:40:21 +0000

    edit items_controller

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,17 @@
 class Admin::ItemsController < ApplicationController
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+  end
+
+  private
+    def item_params
+      params.require(:item).permit(:name)
+    end
+
 end
