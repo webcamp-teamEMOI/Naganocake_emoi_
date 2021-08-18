734724db43abfbe6379223f9a08d64f2e8f43410
AyanoOkamuro <omochi185@gmail.com>
2021-08-18 07:56:27 +0000

    edit top view homes_controller

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -16,7 +16,7 @@ class Admin::ItemsController < ApplicationController
 
   private
     def item_params
-      params.require(:item).permit(:genre_id, :name, :image_id, :introduction, :prace, :is_active)
+      params.require(:item).permit(:genre_id, :name, :image, :introduction, :price, :is_active)
     end
 
 end
