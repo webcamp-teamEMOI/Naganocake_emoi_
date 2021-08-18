a5b8a3bca840b6018cb7e1c5328644646993d250
ichiyanagi7 <ichiyanagi1024@gmail.com>
2021-08-18 02:53:37 +0000

    管理者側 会員一覧画面

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,26 @@
 class Admin::CustomersController < ApplicationController
+
+  def index
+    @customers=Customer.all.page(params[:page]).per(10)
+  end
+
+  def show
+    @customer=Costomer.find(params[:id])
+  end
+
+  def update
+    customer=Costomer.find(params[:id])
+    if customer.update(customer_params)
+      redirect_to request.referer
+    else
+      redirect_to request.referer,notice:"更新できませんでした"
+    end
+  end
+
+  protected
+
+  def customer_params
+    params.require(:customer).permit(:is_deleted)
+  end
+
 end
