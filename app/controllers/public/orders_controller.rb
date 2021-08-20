41ffccd50af2b5d7117e06d8d26af0ecae991782
YukiMiyakawa <sakomiya3838@gmail.com>
2021-08-20 07:57:45 +0000

    orders_new

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,9 +1,20 @@
 class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
-    @customer = Customer.find(1)
+    @customer = current_customer
+    @addresses = Address.where(customer_id: current_customer.id)
+    # @customer = current_customer
+    # @customer = Customer.find(params[:current_customer])
   end
 
   def comfirm
+    @order = Order.new.params(order_params)
+
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:shipping_cost, :total_payment, :payment_method, :name, :address, :postal_code, :status)
   end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :customer
+
+   def address_all
+   "〒" + self.postal_code + " " + self.address + " " + self.name
+  end
 end
