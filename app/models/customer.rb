b188993043dcb98046d0033e95958af939c63c2f
tak-dwc <taku3277@gmail.com>
2021-08-22 14:44:55 +0000

    tweak_customer_views

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -20,5 +20,12 @@ class Customer < ApplicationRecord
   def active_for_authentication?
     super && (self.is_deleted == false)
   end
-
+  
+  validates :last_name ,presence: true
+  validates :first_name ,presence: true
+  validates :last_name_kana,presence: true
+  validates :first_name_kana ,presence: true
+  validates :postal_code ,presence: true
+  validates :address ,presence: true
+  validates :phone_number ,presence: true
 end
