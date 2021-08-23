358c142299d76ac33fe60a2545d488c48709a829
tak-dwc <taku3277@gmail.com>
2021-08-23 13:56:10 +0000

    バリデーション修正とテスト確認

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -10,25 +10,23 @@ class Public::CustomersController < ApplicationController
   def update
     @customer = current_customer
     if @customer.update(customer_params)
-    flash[:success] = "登録情報を変更しました。"
     redirect_to customers_my_page_path
     else
     render :edit
     end
   end
-  
+
   def unsubscribe
   @customer = current_customer
   end
-  
+
   def withdraw
    @customer = current_customer
    @customer.update(is_deleted: true)
     reset_session
-    flash[:notice] = "ありがとうございました。またのご利用を心よりお待ちしております。"
     redirect_to root_path
-  
-  end   
+  end
+
 private
   def customer_params
    params.require(:customer).permit(:last_name,:first_name,:last_name_kana,:first_name_kana,:postal_code,:address,:phone_number,:email)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def errors_hash(resource)
+    resource.errors.messages.map do |col, errors_array|
+      [col, errors_array.map { |error| resource.class.human_attribute_name(col) + error }]
+    end.to_h
+  end
 end
