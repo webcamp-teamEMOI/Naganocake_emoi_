7b4041c6e3b0dc11701ad2c0449190dd8c86f03d
tak-dwc <taku3277@gmail.com>
2021-08-24 04:47:42 +0000

    test

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -10,8 +10,10 @@ class Public::CustomersController < ApplicationController
   def update
     @customer = current_customer
     if @customer.update(customer_params)
+    flash[:notice] = "更新できました。"
     redirect_to customers_my_page_path
     else
+    flash[:alert] = "更新できませんでした。記載内容に謝りがある可能性がございます。" 
     render :edit
     end
   end
