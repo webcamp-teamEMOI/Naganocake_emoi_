b83e9bfaab25d1d689d8304a34cbc3fadc59c9cc
tak-dwc <taku3277@gmail.com>
2021-08-24 05:17:50 +0000

    test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
-  　def errors_hash(resource)
+  def errors_hash(resource)
     resource.errors.messages.map do |col, errors_array|
       [col, errors_array.map { |error| resource.class.human_attribute_name(col) + error }]
-    　end.to_h
-　 end
+     end.to_h
+  end
 end
