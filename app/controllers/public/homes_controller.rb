f13779ad1900efe92a9abc91d243c87626c0f76a
AyanoOkamuro <omochi185@gmail.com>
2021-08-25 08:26:32 +0000

    change top view

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,7 @@
 class Public::HomesController < ApplicationController
     def top
-        @genres = Genre.all
-        @items = Item.order("created_at DESC").page(params[:page]).per(4)
+      @genres = Genre.all
+      @items = Item.order("created_at DESC").page(params[:page]).per(4)
     end
     
     def about
